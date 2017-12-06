c1e9a642e6c47b928f306c36fc8fa1861f801c97
Jernej <ostrostrelec1@gmail.com>
2017-12-06 10:02:41 +0100

    gitignore

--- katalog-profilov/src/main/java/si/fri/rso/katalogprofilov/SocialNetworkApplication.java
+++
@@ -1,8 +0,0 @@
-package si.fri.rso.katalogprofilov;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("v1")
-public class SocialNetworkApplication extends Application {
-}
--- upravljanje-profilov/src/main/java/si/fri/rso/upravljanjeprofilov/SocialNetworkApplication.java
+++
@@ -1,8 +0,0 @@
-package si.fri.rso.upravljanjeprofilov;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("v1")
-public class SocialNetworkApplication extends Application {
-}
