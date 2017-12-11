66f28af6dffcc372590fa68024bdadfd84003420
Tjasa <tjasa.jagodnik1@gmail.com>
2017-12-11 11:26:18 +0100

    added metrics

--- src/main/java/si/fri/rso/katalogprofilov/KatalogProfilovResource.java
+++ src/main/java/si/fri/rso/katalogprofilov/KatalogProfilovResource.java
@@ -1,7 +1,10 @@
 
 package si.fri.rso.katalogprofilov;
 
+import com.kumuluz.ee.common.runtime.EeRuntime;
+import org.eclipse.microprofile.metrics.annotation.Metered;
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -16,7 +19,11 @@ import java.util.List;
 @ApplicationScoped
 public class KatalogProfilovResource {
 
+    @Inject
+    private RestProperties restProperties;
+
     @GET
+    @Metered
     public Response getAllProfils() {
         List<Profil> profils = Database.getProfils();
         return Response.ok(profils).build();
@@ -42,5 +49,38 @@ public class KatalogProfilovResource {
     public Response deleteProfil(@PathParam("profilId") String profilId) {
         Database.deleteProfil(profilId);
         return Response.ok(Response.Status.OK).build();
+    }
+
+    @POST
+    @Path("healthy")
+    public Response setHealth(Boolean healthy) {
+        restProperties.setHealthy(healthy);
+        return Response.ok().build();
+    }
+
+    @POST
+    @Path("load")
+    public Response loadOrder(Integer n) {
+
+        for (int i = 1; i <= n; i++) {
+            fibonacci(i);
+        }
+
+        return Response.status(Response.Status.OK).build();
+    }
+
+    private long fibonacci(int n) {
+        if (n <= 1) return n;
+        else return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    @GET
+    @Path("instanceid")
+    public Response getInstanceId() {
+
+        String instanceId =
+                "{\"instanceId\" : \"" + EeRuntime.getInstance().getInstanceId() + "\"}";
+
+        return Response.ok(instanceId).build();
     }
 }
